ee41b60a657e3aac66e0c81d4d907807ad69f326
pakimova0-бот <mypakimova0@gmail.com>
2026-02-11 23:30:30 +0300

    Resolved merge conflicts

---
+++ 02_lesson.py/lesson_2_task_2.py
@@ -0,0 +1,14 @@
+def is_year_leap(year):
+
+    if year % 4 == 0:
+
+       return True
+
+    else:
+
+        return False
+    
+    chosen_year == 1987
+    result= is_year_leap (year to check)
+
+    print(f"1987 {chosen_year}:{resilt}")
