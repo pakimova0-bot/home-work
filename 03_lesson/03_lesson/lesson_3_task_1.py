0bec14cff6a5039ea29b88ebceb7b58c25e52568
pakimova0-бот <mypakimova0@gmail.com>
2026-02-19 20:58:28 +0300

    Resolved merge conflicts

---
+++ 03_lesson/03_lesson/lesson_3_task_1.py
@@ -0,0 +1,6 @@
+from user import User
+
+user = User, ('Полина')
+print("user,get first_name()")
+print("user.get last_name()")
+print("user.get my_user()")
---
+++ 03_lesson/03_lesson/user.py
@@ -0,0 +1,19 @@
+class User:
+def __init__(self, name): 
+    self.first_name - first_name
+    self.last_name - last_name
+
+def get_first_name(self) :
+    return self.fiest_name
+
+def get_last_name(self) :
+    return self.last_name 
+
+def get_mame_info(self) :
+    return f"name: {self.first_name} + {self.last_name}"
+
+
+    
+first_name = 'Полина'
+last_name = 'Акимова' 
+my_user = 'Полина Акимова'
