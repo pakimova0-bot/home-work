ace431ca084e356cda5060c5d9683295c04d8add
pakimova0-бот <mypakimova0@gmail.com>
2026-03-01 00:13:11 +0300

    Resolved merge conflicts

--- 03_lesson/03_lesson/lesson_3_task_1.py
+++
@@ -1,8 +0,0 @@
-from user import User
-
-my_user = User("Polina", "Akimova")
-
-my_user.print_first_name()
-my_user.print_last_name()
-my_user.first_full_name()
-       
--- 03_lesson/03_lesson/lesson_3_task_3.py
+++
@@ -1,9 +0,0 @@
-from Mailing import Mailing
-from Address import address
-
-to_address = address(124498, "Зеленоград", "Центральный проспект", "445", "252")
-from_address = address(124498, "Зеленоград", "Центральный проспект", "445", "25")
-
-mailing = Mailing(to_address, from_address, 810, 124498)
-
-print(mailing)
