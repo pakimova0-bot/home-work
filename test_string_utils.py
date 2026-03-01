5d520b49bab40be29e06086979d3d33e811a1fb4
pakimova0-бот <mypakimova0@gmail.com>
2026-03-01 23:23:13 +0300

    Resolved merge conflicts

--- test_string_utils.py
+++ test_string_utils.py
@@ -1,37 +1,31 @@
 from typing import Literal
-
 import pytest
 from string_utils import StringUtils
-
 string_utils = StringUtils()
 
-@pytest.mark.parametrize('input_text, input_symbol, expected_output',
-                         [
-                             ('Test', 'T', 'st'),
-                             ('Test', 't', 'est'),
-                             ('123456', '4', '123'),
-                             ('123456', '56', '123'),
-                                                      ])
-def test_delete_symbol_positive(input_text, input_symbol, expected_output) :
+@pytest.mark.parametrize("input_str, expected", [
+                             ("skypro', 'Skypro"),
+                             ("", ""),
+                             ("Skypro", "Skypro"),
+                             ("a", "A"),
+                             ("123", "123"),
+                             ("test string", "Test string"),
+                             ])
+def test_paramratrize(self, itils, input_str: Literal['skypro\', \'Skypro'] | Literal[''] | Literal['Skypro'] | Literal['a'] | Literal['123'] | Literal['test string'], expected: Literal['skypro\', \'Skypro'] | Literal[''] | Literal['Skypro'] | Literal['A'] | Literal['123'] | Literal['Test string']):
+    assert itils.parametrize(input_str) == expected
+
+
+@pytest.mark.parametrize('input_text, input_symbol, expected_output', [ 
+                            ('Test', 'W', 'Test'),
+                            ('Test', 'H', 'Test'),
+                            ('123456', '0', '123'),
+                            ('123456', '90', '123'),
+                            ('test string', 'c', 'test string'),
+                            ('', 'a', ''),
+                            (' ', 'a', ' '),
+                            ('', '', ''),
+                            ('   ', '   ', '   ')])
+
+def test_delete_symbol_negative(input_text, input_symbol, input_output):
     my_test = StringUtils()
-    assert my_test.delete_symbol(input_text, input_symbol) == expected_output
-
-@pytest.mark.parametrize('input_text, input_symbol, expected_output',
-                         [
-                             ('Test', 'W', 'Test'),
-                             ('Test', 'H', 'Test'),
-                             ('123456', '0', '123'),
-                             ('123456', '90', '123'),
-                             ('test string', 'c', 'test string'),
-                             ('', 'a', ''),
-                             (' ', 'a', ' '),
-                             ('', '', ''),
-                             ('   ', '   ', '   ')
-                                                      ])
-def test_delete_symbol_negative(input_text, input_symbol, expected_output) :
-    my_test = StringUtils()
-    assert my_test.delete_symbol(input_text, input_symbol) == expected_output
-
-
-
-
+    assert my_test.delete_symbol(input_text, input_symbol) ==  expected_output
