981cc51453e39f4f22708f84932f8764e2f41e45
pakimova0-бот <mypakimova0@gmail.com>
2026-03-14 20:15:01 +0300

    Resolved merge conflicts

--- 05_lesson/lesson05_task1.py
+++ 05_lesson/lesson05_task1.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("http://uitestingplayground.com/classattr.")
 
-driver.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
+driver = webdriver.Chrome()
+driver.get("http://uitestingplayground.com/classattr")
+
+driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
 
 driver.quit()
